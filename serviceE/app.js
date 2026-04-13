b5cc5e9458d7dcd73010817e87025a2f68b48d71
muhammad waqas <mwaqas@gmail.com>
2026-04-13 22:52:28 +0500

    hg2

--- serviceE/app.js
+++ serviceE/app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3005;
 
 app.use(cors());
 
